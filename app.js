ad94e2521d5b9bdf86c3d58b3a1fce0e52dac6fa
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-07-06 16:15:14 +0700

    modified app.yaml to fix deployment bug and change port to 8080, also fix firebase admin modules

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3000;
+const PORT = 8080
 const myRouter = require('./route/routes')
 
 // [*]  MIDDLEWARE [just put middleware on app.js]
